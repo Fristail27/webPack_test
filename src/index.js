7f71234de65c793c232607ff65fdd1eae399136b
Fristail27 <fristail27@gmail.com>
2021-04-25 22:10:58 +0300

    add xml and scv loaders

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import Post from './post'
 import json from './assets/json.json'
 import './styles/styles.css'
 import WebpackLogo from './assets/polar_bear_PNG23526.png'
+import xml from './assets/data.xml'
+import csv from './assets/data.csv'
 
 const post = new Post('webpack post title', WebpackLogo)
 
 console.log('post to String', post.toString())
 
 console.log(json)
+console.log(xml)
+console.log(csv)
--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,15 @@ module.exports = {
             {
                 test: /\.(ttf|woff|woff2|eot)$/,
                 use: ['file-loader']
-            }
+            },
+            {
+                test: /\.xml$/,
+                use: ['xml-loader']
+            },
+            {
+                test: /\.csv$/,
+                use: ['csv-loader']
+            },
         ]
     }
 }

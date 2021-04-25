80973492c420dfb8c683991766d7bc73deff6829
Fristail27 <fristail27@gmail.com>
2021-04-25 21:44:42 +0300

    loaders

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import Post from './post'
+import json from './assets/json.json'
+import './styles/styles.css'
+import WebpackLogo from './assets/polar_bear_PNG23526.png'
 
-const post = new Post('webpack post title')
+const post = new Post('webpack post title', WebpackLogo)
 
 console.log('post to String', post.toString())
+
+console.log(json)

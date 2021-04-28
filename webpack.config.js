fde9372552713b989d5a3ef0d55664c547476359
Fristail27 <Fristail27@gmail.com>
2021-04-28 21:54:18 +0300

    BundleAnalyzerPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const CopyWebpackPlugin = require('copy-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const OptimizeCssAssetsWebpackPlugim = require('optimize-css-assets-webpack-plugin')
 const TerserWebpackPlugin = require('terser-webpack-plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 const isDev = process.env.NODE_ENV === 'development'
 const isProd = !isDev
@@ -66,6 +67,32 @@ const jsLoaders = () => {
     return loaders
 }
 
+const plugins = () => {
+    const base = [
+        new HTMLWebpackPlugin({
+            template: "./index.html",
+            minify: {
+                collapseWhitespace: isProd
+            }
+        }),
+        new CleanWebpackPlugin(),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, 'src/favicon.ico'),
+                    to: path.resolve(__dirname, 'dist')
+                }
+            ]}),
+        new MiniCssExtractPlugin({
+            filename: filename('css')
+        })
+    ]
+    if (isProd) {
+        base.push(new BundleAnalyzerPlugin())
+    }
+    return base
+}
+
 module.exports = {
     context: path.resolve(__dirname, 'src'),
     mode: 'development',
@@ -90,26 +117,8 @@ module.exports = {
         open: true,
         hot: isDev
     },
-    devtool: isDev ? 'source-map' : '',
-    plugins: [
-        new HTMLWebpackPlugin({
-            template: "./index.html",
-            minify: {
-                collapseWhitespace: isProd
-            }
-        }),
-        new CleanWebpackPlugin(),
-        new CopyWebpackPlugin({
-            patterns: [
-                {
-                    from: path.resolve(__dirname, 'src/favicon.ico'),
-                    to: path.resolve(__dirname, 'dist')
-                }
-            ]}),
-        new MiniCssExtractPlugin({
-            filename: filename('css')
-        })
-    ],
+    devtool: 'source-map',
+    plugins: plugins(),
     module: {
         rules: [
             {
